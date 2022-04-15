b5240db660df954e0579e7c22fb526c560d14257
metasong <metaseed@gmail.com>
2022-04-14 22:06:03 -0500

    copy to clickboard

---
+++ dev-note/snippet.ts
@@ -0,0 +1,8 @@
+https://developer.chrome.com/docs/extensions/mv3/devtools/
+https://github.com/bahmutov/code-snippets
+https://levelup.gitconnected.com/snippets-in-google-chrome-62636236b766
+import:
+https://gist.github.com/fjenett/cc47a7a72e5db7dd2a45
+https://github.dev/GoogleChrome/chrome-extensions-samples
+
+https://github.com/saifabusaleh/clipboard-history-extension
--- src/content_script.tsx
+++ src/content_script.tsx
@@ -9,18 +9,42 @@
 //   }
 // });
 
-var port = chrome.runtime.connect({name: "wand"});
+var port = chrome.runtime.connect({ name: "wand" });
 // port.postMessage({joke: "Knock knock"});
-port.onMessage.addListener(function(msg) {
+port.onMessage.addListener(function (msg) {
   // console.log("Receive msg = ", msg);
-  if(msg.code) {
+  if (msg.code) {
+    // https://developer.chrome.com/docs/extensions/mv3/mv2-sunset/
+    // https://developer.chrome.com/docs/extensions/mv3/intro/mv3-migration/#code-execution
+    // https://github.com/Tampermonkey/tampermonkey/issues/644
+    // https://github.com/violentmonkey/violentmonkey/issues/505
     // https://stackoverflow.com/questions/9515704/use-a-content-script-to-access-the-page-context-variables-and-functions/9517879#answer-9517879
-    const eventName = 'onreset';
-    document.documentElement.setAttribute(eventName, msg.code);
-    document.documentElement.dispatchEvent(new CustomEvent('reset'));
-    document.documentElement.removeAttribute(eventName); 
+    // problem: scripts are accumulated! seems remove attribute is not working
+    // const eventName = "onreset";
+    // document.documentElement.setAttribute(eventName, msg.code);
+    // document.documentElement.dispatchEvent(new CustomEvent("reset"));
+    // document.documentElement.removeAttribute(eventName);
+    window.addEventListener(
+      "focus",
+      () => {
+        if (msg.code) {
+          navigator.clipboard.writeText(msg.code).then(
+            function () {
+              console.log("Copied code to clipboard");
+            },
+            function (err) {
+              console.error("Could not copy code to clipboard");
+            }
+          );
+        }
+      },
+      { once: true }
+    );
 
+    // not working
+    // var script = document.createElement("script");
+    // script.textContent = msg.code;
+    // (document.head || document.documentElement).appendChild(script);
+    // script.remove();
   }
 });
-
-
